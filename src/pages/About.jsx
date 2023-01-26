039a282e6ffa5910f72dab0ad173b8c26ba80b8b
Eya ben attig <eyabenattig@gmail.com>
2023-01-26 15:28:04 +0100

    add all my file

---
+++ src/pages/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from "../components/Logo";
+const About = (props) => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi hic atque,
+                 dolorum quae perferendis ratione nihil. At natus, 
+                assumenda quibusdam cum exercitationem porro deleniti debitis vero magni reiciendis commodi? Nemo!
+                orem ipsum dolor sit amet consectetur, adipisicing elit. Nisi hic atque,
+                 dolorum quae perferendis ratione nihil. At natus, 
+                assumenda quibusdam cum exercitationem porro deleniti debitis vero magni reiciendis commodi? Nemo!
+                orem ipsum dolor sit amet consectetur, adipisicing elit. Nisi hic atque,
+                 dolorum quae perferendis ratione nihil. At natus, 
+                assumenda quibusdam cum exercitationem porro deleniti debitis vero magni reiciendis commodi? Nemo!
+            </p>
+            <br />
+
+        </div>
+    );
+};
+
+export default About;
